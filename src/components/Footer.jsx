3ff7a8a79c983241afc69abc1c94922eb0a7205a
rodrigo vargas <rodrigo.vargas11.12@gmail.com>
2022-09-14 20:19:25 -0300

    aplico react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/footer.css";
+import facebook from "../assets/facebook.svg";
+import instagram from "../assets/instagram.svg";
+import youtube from "../assets/youtube.svg";
+import twitter from "../assets/twitter.svg";
+
+function Footer() {
+  return (
+    <footer>
+      <div class="w-100 px-5 foto">
+        {/* <!---- ---Redes  --------> */}
+        <div class="row margen">
+          <div class="col-xl-6 col-lg-6 col-md-9 col-sm-8 col-8 text-center  ">
+            <h1 class="chico">¡SEGUINOS EN NUESTRAS REDES SOCIALES!</h1>
+            <div class="row justify-content-center ">
+              <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 mt-2">
+                <img class="icon-size" src={facebook} alt="" />
+              </div>
+              <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 mt-2">
+                <img class="icon-size" src={instagram} alt="" />
+              </div>
+              <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 mt-2">
+                <img class="icon-size" src={twitter} alt="" />
+              </div>
+              <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 mt-2">
+                <button
+                  target="_blank"
+                  href="https://developer.mozilla.org/es/docs/Web/HTML/Element/a"
+                >
+                  {" "}
+                  <img class="icon-size" src={youtube} alt="" />{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* <!---- ---Redes fin --------> */}
+
+          {/* Satuario animal  */}
+          <div class="col-xl-4 col-lg-4   arreglo">
+            <h2 class="chico">SANTUARIO ANIMAL</h2>
+            <ul>
+              <li>
+                <button href=""> Sobre Nosotros</button>
+              </li>
+              <li>
+                <button href=""> Adopciones</button>
+              </li>
+              <li>
+                <button href="">Cómo colaborar</button>
+              </li>
+              <li>
+                <button href=""> Tienda</button>
+              </li>
+              <li>
+                <button href=""> Contacto</button>
+              </li>
+            </ul>
+          </div>
+          {/* <!---- ---Santuario animal fin ------ --> */}
+
+          {/* /*Donar  */}
+
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-2 col-4   text-center ">
+            <button href="" class="botoon">
+              {" "}
+              Donar
+            </button>
+          </div>
+          {/* /* Donar fin  */}
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
