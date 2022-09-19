25ef3b1028b538ffb2012616c93b1b2ed98b1de5
daniel <santoss.rdaniel@gmail.com>
2022-09-19 11:35:22 -0300

    [ADD] ultimos cambioooos

--- src/components/Carouselsantuario.jsx
+++ src/components/Carouselsantuario.jsx
@@ -2,7 +2,6 @@ import Carousel from "react-bootstrap/Carousel";
 import slide1 from "../assets/slide1.jpg";
 import slide2 from "../assets/slide2.jpg";
 import slide3 from "../assets/slide3.jpg";
-import slide11 from "../assets/slide1.1.jpg";
 import "../styles/carouselsantuario.css";
 
 function Carouselsantuario() {
